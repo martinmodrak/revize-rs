f2f775beff43506d451bfc1d7bb6f032af540239
martin_cerny <cerny.m@gmail.com>
2019-03-06 20:53:58 +0100

    Random efekty

--- R/simulator.R
+++ R/simulator.R
@@ -9,30 +9,50 @@ int ordered_logit_rng(real eta, vector c) {
 }
 '
 functions_to_expose <- stanc(model_code = functions_to_expose, model_name = "functions_to_expose")
-# NOT RUN {
 expose_stan_functions(functions_to_expose)
 
-# }
 
-
-simulate_data <- function(N, ncat, N_monotonic, N_monotonic_cat = 4) {
-  intercept_sigma = 1
+simulate_data <- function(N, ncat, N_random_groups, N_monotonic, N_monotonic_cat = 4)  {
+  intercept_sigma <- 1
 
   b <- rnorm(1, 0, 1)
   intercept <- sort(rt(ncat - 1, 3) * intercept_sigma)
 
   X <- rnorm(N, 0, 1)
 
-  X_monotonic = array(sample(1:N_monotonic_cat, N * N_monotonic, replace = TRUE), c(N, N_monotonic))
+  X_monotonic <- array(sample(1:N_monotonic_cat, N * N_monotonic, replace = TRUE), c(N, N_monotonic))
 
-  b_monotonic = rnorm(N_monotonic, 0, 1)
-  zeta_monotonic = MCMCpack::rdirichlet(N_monotonic, rep(1, length.out = N_monotonic_cat))
+  b_monotonic <- rnorm(N_monotonic, 0, 1)
+  zeta_monotonic <- MCMCpack::rdirichlet(N_monotonic, rep(1, length.out = N_monotonic_cat - 1))
 
-  mu <-  X * b;
+  N_random <- length(N_random_groups)
+  random_hyper_sigma <- rep(1, N_random)
+  sigma_random <- abs(rnorm(N_random, 0, random_hyper_sigma))
+
+  b_random_group_start <- cumsum(N_random_groups) - N_random_groups + 1
+  b_random_to_group <- integer(sum(N_random_groups))
+  for(i in 1:N_random) {
+    for(k in 1:N_random_groups[i]) {
+      b_random_to_group[b_random_group_start[i] + k - 1] <- i
+    }
+  }
+
+  b_random <- rnorm(sum(N_random_groups), 0, sigma_random[b_random_to_group])
+
+  X_random_groups <- array(NA_real_, c(N, N_random))
+  for(i in 1:N_random) {
+    X_random_groups[,i] <- sample(1:N_random_groups[i], N, replace = TRUE)
+  }
+  X_random <- array(rbinom(N_random * N, 1, prob = 0.3), c(N_random, N))
+
+  mu <- X * b;
   for(n in 1:N) {
     for(N_m in 1:N_monotonic) {
-      mu[n] <- mu[n] + b_monotonic[N_m] * sum(zeta_monotonic[N_m, 1:X_monotonic[n,N_m]])
+      if(X_monotonic[n, N_m] > 1) {
+        mu[n] <- mu[n] + b_monotonic[N_m] * sum(zeta_monotonic[N_m, 1:(X_monotonic[n,N_m] -1)])
+      }
     }
+    mu[n] <- mu[n] + sum(X_random[,n] * b_random[X_random_groups[n,]])
   }
 
   Y <- purrr::map_dbl(mu, ordered_logit_rng, intercept)
@@ -45,6 +65,11 @@ simulate_data <- function(N, ncat, N_monotonic, N_monotonic_cat = 4) {
       N_monotonic = N_monotonic,
       N_monotonic_cat = N_monotonic_cat,
       X_monotonic = X_monotonic,
+      N_random = N_random,
+      N_random_groups = N_random_groups,
+      X_random = X_random,
+      X_random_groups = X_random_groups,
+      random_hyper_sigma = random_hyper_sigma,
       Y = Y,
       intercept_sigma = 1
     ),
@@ -52,6 +77,8 @@ simulate_data <- function(N, ncat, N_monotonic, N_monotonic_cat = 4) {
       b = b,
       b_monotonic = b_monotonic,
       zeta_monotonic = zeta_monotonic,
+      b_random = b_random,
+      sigma_random = sigma_random,
       intercept = intercept
     )
   )
--- stan/main_model.stan
+++ stan/main_model.stan
@@ -20,14 +20,39 @@ data {
   vector[N] X;
   real<lower=0> intercept_sigma;
 
-  //Monotonic  effects
+  //Monotonic fixed effects
   int<lower=0> N_monotonic;
   int<lower=2> N_monotonic_cat;
   int<lower=1, upper = N_monotonic_cat> X_monotonic [N, N_monotonic];
+
+  //Random effects
+  int<lower=0> N_random;
+  int<lower=0> N_random_groups[N_random];
+  int<lower=0, upper = max(N_random_groups)> X_random_groups[N, N_random];
+  matrix[N_random, N] X_random;
+  vector[N_random] random_hyper_sigma;
 }
 
 transformed data {
   //TODO: maybe check that predictors are centered
+  int<lower=0> b_random_group_start[N_random];
+  int<lower=0> b_random_to_group[sum(N_random_groups)];
+  int<lower=0> b_random_index[N, N_random];
+
+  b_random_group_start[1] = 1;
+  for(i in 2:N_random) {
+    b_random_group_start[i] = b_random_group_start[i - 1] + N_random_groups[i - 1];
+  }
+  for(i in 1:N_random) {
+    for(k in b_random_group_start[i]:(b_random_group_start[i] + N_random_groups[i] - 1)) {
+      b_random_to_group[k] = i;
+    }
+
+    for(n in 1:N) {
+      b_random_index[n, i] = b_random_group_start[i] + X_random_groups[n, i] - 1;
+    }
+
+  }
 }
 
 parameters {
@@ -37,26 +62,44 @@ parameters {
 
   //Monotonic effects
   vector[N_monotonic] b_monotonic;
-  simplex[N_monotonic_cat] zeta_monotonic[N_monotonic];
+  simplex[N_monotonic_cat - 1] zeta_monotonic[N_monotonic];
+
+  //Random effects
+  vector[sum(N_random_groups)] b_random_raw;
+  vector[N_random] sigma_random;
 }
 
 transformed parameters {
   matrix[N_monotonic_cat, N_monotonic] b_monotonic_trans;
+
+  vector[sum(N_random_groups)] b_random;
+
   for(n in 1:N_monotonic) {
-    b_monotonic_trans[,n] = b_monotonic[n] * cumsum(zeta_monotonic[n]);
+    b_monotonic_trans[1, n] = 0;
+    b_monotonic_trans[2:N_monotonic_cat, n] = b_monotonic[n] * cumsum(zeta_monotonic[n]);
   }
+
+  b_random = b_random_raw .* sigma_random[b_random_to_group];
 }
 
 model {
   vector[N] mu;
   for(n in 1:N) {
-    mu[n] = X[n] * b + sum(diagonal(b_monotonic_trans[X_monotonic[n,]]));
+    vector[N_monotonic] mon;
+    for(n_m in 1:N_monotonic) {
+      mon[n_m] = b_monotonic_trans[X_monotonic[n, n_m], n_m];
+    }
+    mu[n] = X[n] * b + sum(mon) +//sum(diagonal(b_monotonic_trans[X_monotonic[n,]])) +
+      sum(X_random[,n] .* b_random[b_random_index[n,]]);
   }
   // priors
   //target += student_t_lpdf(intercept | 3, 0, 10);
   intercept ~ student_t(3, 0, intercept_sigma);
   b ~ normal(0, 1);
   b_monotonic ~ normal(0, 1);
+
+  b_random_raw ~ normal(0, 1);
+  sigma_random ~ normal(0, random_hyper_sigma);
 
   // likelihood
   //target += ordered_logistic_lpmf(Y[n] | mu[n], intercept);
