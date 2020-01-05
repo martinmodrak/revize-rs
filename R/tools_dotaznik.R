835fe6ab4da057de1025ea2e31ef185539a7a1ef
martin_cerny <cerny.m@gmail.com>
2020-01-05 18:48:46 +0100

    Slucovani doplnek s hlavnim dotaznikem

--- R/tools_dotaznik.R
+++ R/tools_dotaznik.R
@@ -28,6 +28,94 @@ nacti_dotaznik <- function() {
     left_join(doplnek, by = c("session" = "session"), suffix = c("",".doplnek")) %>%
     rename(ended.rozcestnik = ended)
 
+  cela_data %>% as_tibble()
+}
+
+preprocess_dat <- function(cela_data, verbose = TRUE) {
+  cela_data <-  cela_data %>%
+    mutate(zvolena_delka_dotazniku = factor(kolik_casu, levels = c(1,2), labels = c("kratka", "plna")))
+
+  # Odstranit background
+  for(sloupec in names(cela_data)) {
+    if(grepl("^background", sloupec)) {
+      #cela_data <- cela_data %>% mutate(select(- !!sloupec))
+      if(verbose) {
+        cat("Odstranuji ", sloupec, "\n")
+      }
+      cela_data <- within(cela_data, rm(list = sloupec))
+    }
+  }
+
+
+  neslucovane_sloupce <- c("created", "modified", "ended","expired", "kompetence_k_zobrazeni")
+  kopirovane_sloupce <- c("kolik_casu", "kategorie_respondenta", "bez_zkusenosti_mladsi")
+
+  # Sloucit .doplnek a puvodni sloupec
+  cela_data_backup <- cela_data
+  for(sloupec in names(cela_data)) {
+    sloupec_doplnek <- paste0(sloupec, ".doplnek")
+    if(sloupec_doplnek %in% names(cela_data)) {
+      if(any(!is.na(cela_data[[sloupec_doplnek]]) & cela_data$zvolena_delka_dotazniku == "delsi")) {
+        stop(paste0("Vyplneno ", sloupec_doplnek, " i když si vybral delší verzi"))
+      }
+
+      if(sloupec %in% neslucovane_sloupce) {
+        next;
+      } else if(sloupec %in% kopirovane_sloupce) {
+        if(any(!is.na(cela_data[[sloupec]]) & !is.na(cela_data[[sloupec_doplnek]]) &
+               cela_data[[sloupec]] != cela_data[[sloupec_doplnek]])) {
+          stop("Kopirovany sloupec ", sloupec, " se neshoduje.")
+        }
+        if(verbose) {
+          cat("Odstranuji kopirovany", sloupec_doplnek, "\n")
+        }
+        cela_data <- within(cela_data, rm(list = sloupec_doplnek))
+      } else {
+        if(any(!is.na(cela_data[[sloupec]]) & !is.na(cela_data[[sloupec_doplnek]]))) {
+          stop(paste0("Sloupec ", sloupec, " je vyplnen dvakrat."))
+        }
+        if(verbose) {
+          cat("Slucuji", sloupec, "a", sloupec_doplnek, "\n")
+        }
+
+        cela_data[[sloupec]] <- if_else(is.na(cela_data[[sloupec]]), cela_data[[sloupec_doplnek]], cela_data[[sloupec]])
+
+        cela_data <- within(cela_data, rm(list = sloupec_doplnek))
+      }
+    }
+  }
+
+  # Separatni kontrola, ze slouceni fungovalo
+  n_tests <- 0
+  for(sloupec_doplnek in names(cela_data_backup)) {
+    if(grepl("\\.doplnek$", sloupec_doplnek)) {
+      sloupec <- gsub("\\.doplnek$", "", sloupec_doplnek)
+      if(sloupec %in% neslucovane_sloupce || sloupec %in% kopirovane_sloupce) {
+        next;
+      }
+      na_divne <- is.na(cela_data[[sloupec]]) !=
+        (is.na(cela_data_backup[[sloupec]])
+         & is.na(cela_data_backup[[sloupec_doplnek]]))
+
+      if(verbose) {
+        cat("Kontroluji", sloupec,"\n")
+      }
+      if(any( na_divne)) {
+        stop(paste0("Chybná NA struktura pro ", sloupec, " radky ", paste0(which(na_divne), collapse = ", ")))
+      }
+
+      if(!all(is.na(cela_data[[sloupec]]) | cela_data[[sloupec]] == cela_data_backup[[sloupec]] |
+              cela_data[[sloupec]] == cela_data_backup[[sloupec_doplnek]])) {
+        stop(paste0("Divné hodnoty pro ", sloupec))
+      }
+      n_tests <- n_tests + 1
+    }
+  }
+
+  if(n_tests < 10) {
+    stop("Tesovací kód nefunguje")
+  }
+
 
   #Delka vyplneni
   dates <- cela_data$ended.hlavni %>% ymd_hms()
@@ -36,10 +124,8 @@ nacti_dotaznik <- function() {
   cela_data$pocet_hodin_od_startu <- as.numeric(dates - dotaznik_start)
 
   #Kraje
-  kraje_dle_id <- hlavni_items$kraj$choices %>% unlist()
-  cela_data <- cela_data %>% mutate(kraj = if_else(is.na(kraj), kraj.doplnek, kraj),
-                                    kraj_nazev = kraje_dle_id[kraj])
-
+  cela_data <- cela_data %>% mutate(
+    kraj_nazev = as.factor(kraj))
 
   cela_data
 }
