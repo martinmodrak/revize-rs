5dc0913c3ccaaf04594d04bc084484c18f1a1aee
fidadoma <filip.dechterenko@gmail.com>
2020-01-25 21:16:37 +0100

    pridana faktorova analyza, alenic moc

--- R/tools_dotaznik.R
+++ R/tools_dotaznik.R
@@ -461,3 +461,11 @@ zobraz_fa <- function(fa_res, nfac,cutoff = 0.3,str_to_remove) {
   m <- as_tibble(m, .name_repair = ~paste0("skupina",1:nfac)) %>% mutate(role = str_remove(row_names_fa,str_to_remove)) %>% select(role,everything())
   m
 }
+
+vytvor_promenne_dle_fa <- function(df,role_fa,fa_res, var_name = "role") {
+  for (i in 1:ncol(fa_res$loadings)) {
+    xx <- (role_fa %>% as.matrix()) %*% fa_res$loadings[,i] %>% as.numeric()
+    df[[paste0(var_name,i)]] <- xx # asi by to slo maticove, ale tohle bylo prmocare
+  }
+  df
+}
